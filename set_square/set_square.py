4da0b904795671db9a100106789f41e70d2f1f18
Oliver Köhnen <60791859+Oliver-sehralt@users.noreply.github.com>
2020-10-15 18:09:01 +0200

    Add files via upload

---
+++ set_square/set_square.py
@@ -0,0 +1,10 @@
+def area(radius):
+    return 3.142 * radius * radius
+
+def vol(area, lenght):
+    print(area + length)
+
+radius = int(input('enter a radius:'))
+length = int(input('enter a lenght:'))
+
+vol(area(radius), lenght)
